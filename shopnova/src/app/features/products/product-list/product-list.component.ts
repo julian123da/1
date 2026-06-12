1dc929c5cf2dc5247f9239abb60de61758a186a6
jacevedo6 <jacevedo@electrosoftware.net>
2026-06-12 15:11:55 -0500

    feat: sparkles: fix3

--- shopnova/src/app/features/products/product-list/product-list.component.ts
+++ shopnova/src/app/features/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { ProductCardComponent } from '../../../shared/components/product-card/product-card.component';
 import { ProductService } from '../services/product.service';
 
@@ -13,11 +14,18 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductListComponent {
   private productService = inject(ProductService);
+  private route = inject(ActivatedRoute);
 
   searchTerm = signal('');
   selectedCategory = signal('');
 
   categories = this.productService.getCategories();
+
+  constructor() {
+    this.route.queryParams.subscribe(params => {
+      this.selectedCategory.set(params['category'] ?? '');
+    });
+  }
 
   filtered = computed(() => {
     let products = this.productService.getAll();
